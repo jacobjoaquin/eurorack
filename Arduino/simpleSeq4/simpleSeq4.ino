419be90405c0a666eb84bdab9bd5d66ccd4cf1a3
Jacob Joaquin <jacobjoaquin@gmail.com>
2017-03-28 09:16:05 -0700

    buttonStates

--- Arduino/simpleSeq4/simpleSeq4.ino
+++ Arduino/simpleSeq4/simpleSeq4.ino
@@ -1,4 +1,6 @@
 int buttons[] = {13, 14, 15, 16};
+int buttonStates[] = {1, 1, 0, 0};
+
 int leds[] = {12, 11, 10, 9};
 int triggerIn = 8;
 // int triggerLED = 7;
@@ -25,16 +27,37 @@ void next() {
   for(int i = 0; i < nSteps; i++) {
     digitalWrite(leds[i], LOW);
   }
+
   digitalWrite(leds[position], HIGH);
 
 
   // Turn on output (gate is determined by triggerIn);
-  digitalWrite(triggerOut, HIGH);
+  if (buttonStates[position] == HIGH) {
+    digitalWrite(triggerOut, HIGH);
+  }
 
   while (digitalRead(triggerIn) == HIGH) {};
 
   // Turn off output
   digitalWrite(triggerOut, LOW);
+}
+
+void updateButtonState(int button) {
+
+  buttonStates[button] = !buttonStates[button];
+}
+
+void updateButtonState0() {
+  updateButtonState(0);
+}
+void updateButtonState1() {
+  updateButtonState(1);
+}
+void updateButtonState2() {
+  updateButtonState(2);
+}
+void updateButtonState3() {
+  updateButtonState(3);
 }
 
 void initIO() {
@@ -46,9 +69,15 @@ void initIO() {
     pinMode(buttons[i], INPUT);
     pinMode(leds[i], OUTPUT);
   }
+  attachInterrupt(digitalPinToInterrupt(buttons[0]), updateButtonState0, RISING);
+  attachInterrupt(digitalPinToInterrupt(buttons[1]), updateButtonState1, RISING);
+  attachInterrupt(digitalPinToInterrupt(buttons[2]), updateButtonState2, RISING);
+  attachInterrupt(digitalPinToInterrupt(buttons[3]), updateButtonState3, RISING);
 }
 
+
 void setup() {
+  // Serial.begin(9600);
   initIO();
 }
 
