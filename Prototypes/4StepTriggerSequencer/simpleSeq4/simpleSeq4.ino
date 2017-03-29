f1c9481cdbd761738da59d1cb1d7376a3799f7e4
Jacob Joaquin <jacobjoaquin@gmail.com>
2017-03-29 10:37:37 -0700

    Commented code

--- Prototypes/4StepTriggerSequencer/simpleSeq4/simpleSeq4.ino
+++ Prototypes/4StepTriggerSequencer/simpleSeq4/simpleSeq4.ino
@@ -1,57 +1,22 @@
-int buttons[] = {13, 14, 15, 16};
-int buttonStates[] = {1, 1, 0, 0};
+/*
+simpleSeq4.ino
+Coded by Jacob Joaquin
 
-int leds[] = {12, 11, 10, 9};
-int ledsPWM[] = {20, 21, 22, 23};
+Part of my Eurorack Build Journal
+https://github.com/jacobjoaquin/eurorack
+*/
 
-int triggerIn = 8;
-// int triggerLED = 7;
+const int nSteps = 4;               // Number of steps
+int position = nSteps - 1;          // Current step position
+int buttons[] = {13, 14, 15, 16};   // Pins for buttons
+int buttonStates[] = {1, 1, 1, 1};  // Button states
+int ledsPWM[] = {20, 21, 22, 23};   // Pins for LEDs
+int triggerIn = 8;                  // Pin for Clock Input
+int triggerOut = 6;                 // Pin for Pulse Output
 
-int triggerOut = 6;
 
-int position = 0;
-int nSteps = 4;
-
-ulong waitTime = 60000 / 120;
-ulong nextTrigger = 0;
-
-void wait() {
-  while(millis() < nextTrigger) {}
-
-  nextTrigger = millis() + waitTime;
-}
-
-void next() {
-  // wait();
-  while (digitalRead(triggerIn) == LOW) {}
-
-  position = (position + 1) % nSteps;
-  for(int i = 0; i < nSteps; i++) {
-    digitalWrite(leds[i], LOW);
-    analogWrite(ledsPWM[i], 0);
-  }
-
-  digitalWrite(leds[position], HIGH);
-
-  if (buttonStates[position]) {
-    analogWrite(ledsPWM[position], 255);
-  } else {
-    analogWrite(ledsPWM[position], 8);
-  }
-
-  // Turn on output (gate is determined by triggerIn);
-  if (buttonStates[position] == HIGH) {
-    digitalWrite(triggerOut, HIGH);
-  }
-
-  while (digitalRead(triggerIn) == HIGH) {};
-
-  // Turn off output
-  digitalWrite(triggerOut, LOW);
-}
-
+// Flips the state of the button
 void updateButtonState(int button) {
-
   buttonStates[button] = !buttonStates[button];
 }
 
@@ -68,27 +33,59 @@ void updateButtonState3() {
   updateButtonState(3);
 }
 
-void initIO() {
+void setup() {
+  // Setup jack input and output
   pinMode(triggerIn, INPUT);
   pinMode(triggerOut, OUTPUT);
-  // pinMode(triggerLed, OUTPUT);
 
+  // Setup buttons and LEDs
   for(int i = 0; i < nSteps; i++) {
     pinMode(buttons[i], INPUT);
     pinMode(leds[i], OUTPUT);
   }
+
+  // Attach interrupts that flips button states
   attachInterrupt(digitalPinToInterrupt(buttons[0]), updateButtonState0, RISING);
   attachInterrupt(digitalPinToInterrupt(buttons[1]), updateButtonState1, RISING);
   attachInterrupt(digitalPinToInterrupt(buttons[2]), updateButtonState2, RISING);
   attachInterrupt(digitalPinToInterrupt(buttons[3]), updateButtonState3, RISING);
 }
 
-
-void setup() {
-  // Serial.begin(9600);
-  initIO();
-}
-
 void loop() {
-  next();
+  // Wait for clock in pulse
+  while (digitalRead(triggerIn) == LOW) {}
+
+  // Advance clock
+  position = (position + 1) % nSteps;
+
+  // Clear LEDs
+  for(int i = 0; i < nSteps; i++) {
+    // digitalWrite(leds[i], LOW);
+    analogWrite(ledsPWM[i], 0);
+  }
+
+  // digitalWrite(leds[position], HIGH);
+
+  // Adust brightness of active LED
+  if (buttonStates[position]) {
+    // Bright if played
+    analogWrite(ledsPWM[position], 255);
+  } else {
+    // Dim if current position but not played
+    analogWrite(ledsPWM[position], 8);
+  }
+
+  // Set output to HIGH for active step
+  if (buttonStates[position] == HIGH) {
+    digitalWrite(triggerOut, HIGH);
+  }
+
+  // Wait for clock in signal to go to low
+  while (digitalRead(triggerIn) == HIGH) {};
+
+  // Set output to LOW for active step
+  digitalWrite(triggerOut, LOW);
+
+  // NOTE: The width of the output pulse is derived from the width
+  // of the Clock Input pulse
 }
