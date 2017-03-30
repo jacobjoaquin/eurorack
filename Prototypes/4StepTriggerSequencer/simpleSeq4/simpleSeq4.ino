b0305df805e51519608616b62b3e10de5735d1af
Jacob Joaquin <jacobjoaquin@gmail.com>
2017-03-30 07:30:01 -0700

    updated names and added waitForPulse() function

--- Prototypes/4StepTriggerSequencer/simpleSeq4/simpleSeq4.ino
+++ Prototypes/4StepTriggerSequencer/simpleSeq4/simpleSeq4.ino
@@ -10,50 +10,55 @@ const int nSteps = 4;                       // Number of steps
 int position = nSteps - 1;                  // Current step position
 int buttons[] = {13, 14, 15, 16};           // Pins for buttons
 int ledsPWM[] = {20, 21, 22, 23};           // Pins for LEDs
-int triggerIn = 8;                          // Pin for Clock Input
-int triggerOut = 6;                         // Pin for Pulse Output
+int clockInput = 8;                         // Pin for Clock Input
+int pulseOutput = 6;                        // Pin for Pulse Output
 volatile int stepStates[] = {1, 1, 1, 1};   // Step states (1 = on, 0 = off)
 
+// Waits until an input pin is the the stated level
+void waitForPulse(int pin, int level) {
+  while (digitalRead(pin) != level) {}
+}
+
 // Flips the state of the button
-void toggleButtonState(int button) {
+void toggleState(int button) {
   stepStates[button] = !stepStates[button];
 }
 
-void toggleButtonState0() {
-  toggleButtonState(0);
+void toggleState0() {
+  toggleState(0);
 }
-void toggleButtonState1() {
-  toggleButtonState(1);
+void toggleState1() {
+  toggleState(1);
 }
-void toggleButtonState2() {
-  toggleButtonState(2);
+void toggleState2() {
+  toggleState(2);
 }
-void toggleButtonState3() {
-  toggleButtonState(3);
+void toggleState3() {
+  toggleState(3);
 }
 
 void setup() {
   // Setup jack input and output
-  pinMode(triggerIn, INPUT);
-  pinMode(triggerOut, OUTPUT);
+  pinMode(clockInput, INPUT);
+  pinMode(pulseOutput, OUTPUT);
 
   // Setup buttons
   for(int i = 0; i < nSteps; i++) {
     pinMode(buttons[i], INPUT);
   }
 
-  // Attach interrupts that flips step states
-  attachInterrupt(digitalPinToInterrupt(buttons[0]), toggleButtonState0, RISING);
-  attachInterrupt(digitalPinToInterrupt(buttons[1]), toggleButtonState1, RISING);
-  attachInterrupt(digitalPinToInterrupt(buttons[2]), toggleButtonState2, RISING);
-  attachInterrupt(digitalPinToInterrupt(buttons[3]), toggleButtonState3, RISING);
+  // Attach toggle interrupts for buttons
+  attachInterrupt(digitalPinToInterrupt(buttons[0]), toggleState0, RISING);
+  attachInterrupt(digitalPinToInterrupt(buttons[1]), toggleState1, RISING);
+  attachInterrupt(digitalPinToInterrupt(buttons[2]), toggleState2, RISING);
+  attachInterrupt(digitalPinToInterrupt(buttons[3]), toggleState3, RISING);
 }
 
 void loop() {
   // Wait for clock in pulse
-  while (digitalRead(triggerIn) == LOW) {}
+  waitForPulse(clockInput, HIGH);
 
-  // Advance clock
+  // Advance step position
   position = (position + 1) % nSteps;
 
   // Clear LEDs
@@ -72,14 +77,14 @@ void loop() {
 
   // Set output to HIGH for active step
   if (stepStates[position] == HIGH) {
-    digitalWrite(triggerOut, HIGH);
+    digitalWrite(pulseOutput, HIGH);
   }
 
   // Wait for clock in signal to go to low
-  while (digitalRead(triggerIn) == HIGH) {};
+  waitForPulse(clockInput, LOW);
 
   // Set output to LOW for active step
-  digitalWrite(triggerOut, LOW);
+  digitalWrite(pulseOutput, LOW);
 
   // NOTE: The width of the output pulse is derived from the width
   // of the Clock Input pulse
