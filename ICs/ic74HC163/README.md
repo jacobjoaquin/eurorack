da7d696e187501259e7667057f5a95fcba8c2ed4
Jacob Joaquin <jacobjoaquin@gmail.com>
2017-04-01 08:34:30 -0700

    Initial commit

---
+++ ICs/ic74HC163/README.md
@@ -0,0 +1,9 @@
+# 74HC163
+
+The 74HC163 is a presettable synchronous 4-bit binary counter. This example simply counts from 0 through 15, and can be manually reset back to 0. The data inputs on the IC are unused.
+
+- The Ardunio sends out a pulse to increment the count.
+- LEDs display the current value of the counter.
+- The button resets the counter back to 0.
+
+![foo](./assets/ic74HC163_bb.png)
---
+++ ICs/ic74HC163/ic74HC163.ino
@@ -0,0 +1,29 @@
+const byte buttonPin = 2;
+const byte resetPin = 12;
+const byte clockPin = 13;
+const int delayTime = 100;
+unsigned long waitTime = 0;
+
+void resetClock() {
+  digitalWrite(resetPin, LOW);
+  digitalWrite(clockPin, LOW);
+  delay(5);
+  digitalWrite(clockPin, HIGH);
+  digitalWrite(resetPin, HIGH);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(resetPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  digitalWrite(resetPin, HIGH);
+  pinMode(buttonPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), resetClock, RISING);
+}
+
+void loop() {
+  digitalWrite(clockPin, HIGH);
+  delay(delayTime);
+  digitalWrite(clockPin, LOW);
+  delay(delayTime);
+}
