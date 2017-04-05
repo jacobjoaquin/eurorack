a5b5b698d3fc85703b84c66bc8111bfe7a75394d
Jacob Joaquin <jacobjoaquin@gmail.com>
2017-04-05 08:44:23 -0700

    Divided the processed into 4 separate modes. Buttons disabled.

--- Prototypes/4StepTriggerSequencer/simpleSeq4/simpleSeq4.ino
+++ Prototypes/4StepTriggerSequencer/simpleSeq4/simpleSeq4.ino
@@ -4,22 +4,37 @@ Coded by Jacob Joaquin
 
 Part of my Eurorack Build Journal
 https://github.com/jacobjoaquin/eurorack
+
+NOTE: The width of the output pulse is derived from the width
+of the Clock Input pulse
+
 */
 
-const int nSteps = 4;                         // Number of steps
-const ulong minStateDelay = 50;               // Minimum wait time before state change
-int position = nSteps - 1;                    // Current step position
-int buttons[] = {13, 14, 15, 16};             // Pins for buttons
-int ledsPWM[] = {20, 21, 22, 23};             // Pins for LEDs
-int clockInput = 8;                           // Pin for Clock Input
-int pulseOutput = 6;                          // Pin for Pulse Output
-volatile int stepStates[] = {1, 0, 0, 0};     // Step states (1 = on, 0 = off)
-volatile ulong stateTimers[] = {0, 0, 0, 0};  // Time since last state changes
+const int nSteps = 4;                            // Number of steps
+const uint32_t minStateDelay = 50;               // Minimum wait time before state change
+int position = nSteps - 1;                       // Current step position
+int buttons[] = {13, 14, 15, 16};                // Pins for buttons
+int ledsPWM[] = {20, 21, 22, 23};                // Pins for LEDs
+int clockInput = 8;                              // Pin for Clock Input
+int pulseOutput = 6;                             // Pin for Pulse Output
+volatile int stepStates[] = {1, 0, 1, 0};        // Step states (1 = on, 0 = off)
+volatile uint32_t stateTimers[] = {0, 0, 0, 0};  // Time since last state changes
 
-// Waits until an input pin is the the stated level
-void waitForPulse(int pin, int level) {
-  while (digitalRead(pin) != level) {}
+// Modes
+const int nModes = 4;
+const int modeWaitForHigh = 0;
+const int modeHigh = 1;
+const int modeWaitForLow = 2;
+const int modeLow = 3;
+int mode = modeWaitForHigh;
+
+// Advances to the next mode
+void nextMode() {
+  mode = (mode + 1) % nModes;
 }
+
+// Buttons and Debouncing
+
 
 // Toggles step state
 void toggleState(int index) {
@@ -28,7 +43,6 @@ void toggleState(int index) {
     if (digitalRead(buttons[index]) == HIGH) {
       stepStates[index] = !stepStates[index];
     }
-
   }
 
   // Attempts to debounce button
@@ -57,46 +71,54 @@ void setup() {
   for(int i = 0; i < nSteps; i++) {
     pinMode(buttons[i], INPUT);
   }
-
-  // Attach toggle interrupts for buttons
-  attachInterrupt(digitalPinToInterrupt(buttons[0]), toggleState0, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(buttons[1]), toggleState1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(buttons[2]), toggleState2, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(buttons[3]), toggleState3, CHANGE);
 }
 
 void loop() {
-  // Wait for clock in pulse
-  waitForPulse(clockInput, HIGH);
+  // Check for button press
 
-  // Advance step position
-  position = (position + 1) % nSteps;
 
-  // Clear LEDs
-  for(int i = 0; i < nSteps; i++) {
-    analogWrite(ledsPWM[i], 0);
+  switch (mode) {
+    case modeWaitForHigh:
+    if (digitalRead(clockInput) == HIGH) {
+      nextMode();
+    }
+    break;
+
+    case modeHigh:
+    // Advance step position
+    position = (position + 1) % nSteps;
+
+    // Clear LEDs
+    for(int i = 0; i < nSteps; i++) {
+      analogWrite(ledsPWM[i], 0);
+    }
+
+    // Adust brightness of active LED
+    if (stepStates[position]) {
+      // Bright if played
+      analogWrite(ledsPWM[position], 255);
+    } else {
+      // Dim if current position but not played
+      analogWrite(ledsPWM[position], 8);
+    }
+
+    // Set output to HIGH for active step
+    if (stepStates[position]) {
+      digitalWrite(pulseOutput, HIGH);
+    }
+    nextMode();
+    break;
+
+    case modeWaitForLow:
+    if (digitalRead(clockInput) == LOW) {
+      nextMode();
+    }
+    break;
+
+    case modeLow:
+    // Set output to LOW for active step
+    digitalWrite(pulseOutput, LOW);
+    nextMode();
+    break;
   }
-
-  // Adust brightness of active LED
-  if (stepStates[position]) {
-    // Bright if played
-    analogWrite(ledsPWM[position], 255);
-  } else {
-    // Dim if current position but not played
-    analogWrite(ledsPWM[position], 8);
-  }
-
-  // Set output to HIGH for active step
-  if (stepStates[position]) {
-    digitalWrite(pulseOutput, HIGH);
-  }
-
-  // Wait for clock in signal to go to low
-  waitForPulse(clockInput, LOW);
-
-  // Set output to LOW for active step
-  digitalWrite(pulseOutput, LOW);
-
-  // NOTE: The width of the output pulse is derived from the width
-  // of the Clock Input pulse
 }
