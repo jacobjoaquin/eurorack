d15ba5aa9534c22861b5b82ac15e63611307cd25
Jacob Joaquin <jacobjoaquin@gmail.com>
2017-04-19 08:23:52 -0700

    Internal timer avoids delay()

--- Prototypes/digitalRandomizer/digitalRandomizer.ino
+++ Prototypes/digitalRandomizer/digitalRandomizer.ino
@@ -3,12 +3,18 @@ int randomPin = 13;
 uint32_t d = 5;
 int odds = 128;
 
+uint32_t lastTriggerTime = 0;
+
 void setup() {
   pinMode(randomPin, OUTPUT);
 }
 
 void loop() {
-  d = analogRead(timerPin) / 4;
-  digitalWrite(randomPin, random(256) < odds ? HIGH : LOW);
-  delay(d);
+  // d = analogRead(timerPin) / 4;
+
+  uint32_t now = millis();
+  if (now >= lastTriggerTime + analogRead(timerPin)) {
+    digitalWrite(randomPin, random(256) < odds ? HIGH : LOW);
+    lastTriggerTime = now;
+  }
 }
