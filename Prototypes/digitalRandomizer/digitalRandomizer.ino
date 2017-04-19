cf7e780a1a723f5846ef5f1d4d8e84c65a0a8d6f
Jacob Joaquin <jacobjoaquin@gmail.com>
2017-04-19 08:17:50 -0700

    Added pot to control delay

--- Prototypes/digitalRandomizer/digitalRandomizer.ino
+++ Prototypes/digitalRandomizer/digitalRandomizer.ino
@@ -1,12 +1,14 @@
+int timerPin = A2;
 int randomPin = 13;
-uint32_t d = 250;
-int odds = 64;
+uint32_t d = 5;
+int odds = 128;
 
 void setup() {
   pinMode(randomPin, OUTPUT);
 }
 
 void loop() {
+  d = analogRead(timerPin) / 4;
   digitalWrite(randomPin, random(256) < odds ? HIGH : LOW);
   delay(d);
 }
