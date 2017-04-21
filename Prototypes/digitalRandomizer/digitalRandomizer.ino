328e183319198f183ee12480a38cb243c4e52490
Jacob Joaquin <jacobjoaquin@gmail.com>
2017-04-21 16:51:44 -0700

    Separate control for two types of random out

--- Prototypes/digitalRandomizer/digitalRandomizer.ino
+++ Prototypes/digitalRandomizer/digitalRandomizer.ino
@@ -1,32 +1,39 @@
-int timerPin = A0;
-int oddsPin = A1;
-int randomPin = 13;
-int randomFlipPin = 12;
+int odds0Pin = A0;  // HIGH or LOW
+int odds1Pin = A1;  // Flip-flop
+int random0Pin = 12;
+int random1Pin = 11;
 int flip = false;
 
 int odds = 512;
 uint32_t lastTriggerTime = 0;
 
 void setup() {
-  pinMode(randomPin, OUTPUT);
-  pinMode(randomFlipPin, OUTPUT);
+  pinMode(random0Pin, OUTPUT);
+  pinMode(random1Pin, OUTPUT);
 }
 
 void loop() {
   uint32_t now = millis();
+  // uint32_t timerInput = analogRead(timerPin);
+  uint32_t timerInput = 50;
 
-  if (now >= lastTriggerTime + analogRead(timerPin)) {
+  if (now >= lastTriggerTime + timerInput) {
     lastTriggerTime = now;
-    odds = analogRead(oddsPin);
+    uint32_t odds0 = analogRead(odds0Pin);
+    uint32_t odds1 = analogRead(odds1Pin);
 
-    if (random(1024) < odds) {
-      digitalWrite(randomPin, HIGH);
+    // HIGH or LOW
+    if (random(1024) < odds0) {
+      digitalWrite(random0Pin, HIGH);
 
-      // Flip-flop
-      digitalWrite(randomFlipPin, flip);
-      flip = !flip;
     } else {
-      digitalWrite(randomPin, LOW);
+      digitalWrite(random0Pin, LOW);
+    }
+
+    // Flip-flop
+    if (random(1024) < odds1) {
+      digitalWrite(random1Pin, flip);
+      flip = !flip;
     }
   }
 }
