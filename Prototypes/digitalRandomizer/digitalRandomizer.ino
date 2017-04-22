a19a61f3cc1154f94da1b2e1650cb0149598ae9b
Jacob Joaquin <jacobjoaquin@gmail.com>
2017-04-21 17:11:26 -0700

    Comments

--- Prototypes/digitalRandomizer/digitalRandomizer.ino
+++ Prototypes/digitalRandomizer/digitalRandomizer.ino
@@ -1,13 +1,10 @@
-int odds0Pin = A0;  // HIGH or LOW
-int odds1Pin = A1;  // Flip-flop
-int random0Pin = 12;
-int random1Pin = 11;
-int flip = false;
-
-int odds = 512;
-
-int clockInputPin = 10;
-int lastClockInput = LOW;
+const int clockInputPin = 10;  // Clock input
+const int odds0Pin = A0;       // HIGH or LOW input
+const int odds1Pin = A1;       // Flip-flop input
+const int random0Pin = 12;     // HIGH or LOW pin output
+const int random1Pin = 11;     // Flip-flop pin out
+int flip = false;              // Current state of random1
+int lastClockInput = LOW;      // Last clock input
 
 void setup() {
   pinMode(random0Pin, OUTPUT);
@@ -18,11 +15,12 @@ void setup() {
 void loop() {
   int clockInput = digitalRead(clockInputPin);
 
+  // Trigger next random check when on pulse in.
   if (clockInput == HIGH && clockInput != lastClockInput) {
     uint32_t odds0 = analogRead(odds0Pin);
     uint32_t odds1 = analogRead(odds1Pin);
 
-    // HIGH or LOW
+    // HIGH or LOW Output
     if (random(1024) < odds0) {
       digitalWrite(random0Pin, HIGH);
 
@@ -30,12 +28,12 @@ void loop() {
       digitalWrite(random0Pin, LOW);
     }
 
-    // Flip-flop
+    // Flip-flop Output
     if (random(1024) < odds1) {
       digitalWrite(random1Pin, flip);
       flip = !flip;
     }
   }
-  
+
   lastClockInput = clockInput;
 }
